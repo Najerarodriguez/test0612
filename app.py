a794d2b5d39047a62f07c603156104326f307cea
Najerarodriguez <36144835+Najerarodriguez@users.noreply.github.com>
2023-12-06 09:44:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,27 @@ from firebase_admin import credentials, firestore
 app = Flask(__name__)
 
 # Configura la conexión con Firebase
-cred = credentials.Certificate("path/to/your/credentials.json")
-firebase_admin.initialize_app(cred, {"projectId": "your-project-id"})
+cred = credentials.Certificate("C:\\Users\\root\\Documents\\KAYI\\kayicash20231206\\test20231206-87d2d-firebase-adminsdk-1b922-8b9e62a351.json")
+firebase_admin.initialize_app(cred, {"projectId": "test20231206-87d2d"})
 db = firestore.client()
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# app.py (continuación)
+@app.route("/guardar", methods=["POST"])
+def guardar():
+    numero = float(request.form.get("numero"))
+
+    # Guarda el número en la base de datos Firebase
+    #doc_ref = db.collection("numeros").add({"numero": numero})
+
+    #return f"Número {numero} guardado con éxito con ID: {doc_ref.id}"
+    doc_ref, doc_id = db.collection("numeros").add({"numero": numero})
+    return f"Número {numero} guardado con éxito con ID: {doc_id}"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
